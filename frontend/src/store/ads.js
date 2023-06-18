697b0552b6393239b93c41b6ac42d4a89738aa0f
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-06-19 02:21:00 +0300

    16.2

--- frontend/src/store/ads.js
+++ frontend/src/store/ads.js
@@ -47,31 +47,52 @@ export default {
   },
   mutations: {
     createAd(state, payload){
-        state.ads.push(payload)
-    }
+    state.ads.push(payload)
+  }
 },
 actions: {
-    createAd({commit},payload){
-        payload.id = Math.random()
-        commit('createAd', payload)
-    }
+    async createAd({commit, getters},payload){
+payload.id = Math.random()
+payload.userId = getters.user != null ? getters.user.id : '1'
+commit('clearError')
+commit('setLoading', true)
+//Заглушка запроса
+let isRequestOk = true
+let promise = new Promise(function(resolve) {
+setTimeout(() => resolve('Done')
+, 3000);
+});
+if (isRequestOk) {
+await promise.then(()=> {
+commit('createAd', payload)
+commit('setLoading', false)
+})
+} else {
+await promise.then(()=> {
+commit('setLoading', false)
+commit('setError', 'Ошибка создания объявления')
+throw 'Упс... Ошибка создания объявления'
+})
+}
+  }
+
 },
 getters: {
     ads(state) {
-        return state.ads
+      return state.ads
     },
     promoAds(state) {
-        return state.ads.filter(ad => {
-            return ad.promo
-        })
+      return state.ads.filter(ad => {
+        return ad.promo
+      })
     },
     myAds(state) {
-        return state.ads
-},
-adById(state) {
-return id => {
-return state.ads.find(ad => ad.id == id)
-}
+      return state.ads
+  },
+    adById(state) {
+  return id => {
+  return state.ads.find(ad => ad.id == id)
+  }
 }
 }
 }
