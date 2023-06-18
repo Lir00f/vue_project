9eb208aa5ed0e0e9dd28d05bf94613882f35c2f2
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-06-19 02:26:18 +0300

    17.1

---
+++ frontend/src/views/Ads/EditAdModal.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-dialog
+    v-model="modal"
+    width="400px">
+    <template v-slot:activator="{ props }">
+    <v-btn
+    v-bind="props"
+    color="warning">
+    Изменить</v-btn></template>
+    <v-card class="pa-3">
+    <v-row justify="center">
+    <v-col cols="12">
+    <v-card-title>
+    <h1 class="text--primary">Изменить</h1>
+    </v-card-title>
+    </v-col>
+    </v-row>
+    
+    <v-row justify="center">
+    <v-col cols="12">
+    <v-card-text>
+    Lorem.
+    </v-card-text>
+    </v-col>
+    </v-row>
+    
+    <v-row justify="center">
+    <v-col cols="12">
+    <v-card-actions>
+    <v-spacer></v-spacer>
+    <v-btn>Отменить</v-btn>
+    <v-btn color="success">Сохранить</v-btn>
+    </v-card-actions>
+    </v-col>
+    </v-row>
+    </v-card>
+    </v-dialog>
+</template>
+    
+<script>
+    export default {
+    data() {
+    return {
+    modal: false
+    }
+    }
+    }
+</script>
