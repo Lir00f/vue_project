fa6daa546fc5b19292c1e7fce9a4c8fb7c99f009
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-06-19 02:37:16 +0300

    18.2

--- frontend/src/views/Shared/BuyAdModal.vue
+++ frontend/src/views/Shared/BuyAdModal.vue
@@ -3,24 +3,39 @@
     v-model="modal"
     width="400px">
     <template v-slot:activator="{ props }">
-    <v-btn
-    v-bind="props"
-    color="warning">
-    Купить</v-btn></template>
+        <v-btn
+        v-bind="props"
+        color="warning">
+        Купить</v-btn>
+    </template>
     <v-card class="pa-3">
     <v-row justify="center">
     <v-col cols="12">
     <v-card-title>
-    <h1 class="text--primary">Купить </h1>
+    <h1 class="text--primary">Вы хотите купить?</h1>
     </v-card-title>
     </v-col>
     </v-row>
     
     <v-row justify="center">
     <v-col cols="12">
-    <v-card-text>
-    Временно отсутствует
-    </v-card-text>
+        <v-card-text>
+        <v-text-field
+        name="name"
+        label="Ваше Имя"
+        type="text"
+        v-model="name"
+        >
+        </v-text-field>
+        <v-text-field
+        name="phone"
+        label="Ваш номер телефона"
+        type="text"
+        v-model="phone"
+        >
+        </v-text-field>
+        </v-card-text>
+
     </v-col>
     </v-row>
     
@@ -28,8 +43,8 @@
     <v-col cols="12">
     <v-card-actions>
     <v-spacer></v-spacer>
-    <v-btn>Отмена</v-btn>
-    <v-btn color="success">Купить</v-btn>
+    <v-btn>Закрыть</v-btn>
+    <v-btn color="success">Купить это!</v-btn>
     </v-card-actions>
     </v-col>
     </v-row>
@@ -42,7 +57,10 @@ export default {
 props: ['ad'],
 data() {
     return {
-    modal: false
+    modal: false,
+    name: '',
+    phone: ''
+
     }
 }
 }
