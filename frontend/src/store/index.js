5c8601233e6e41ed86f4f8cd1be41bd1ef6119c7
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-06-19 02:40:27 +0300

    18.5

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,15 @@ import { createStore } from 'vuex'
 import ads from './ads'
 import user from './user'
 import shared from './shared'
+import order from './order'
 
 
 const store = createStore({
 modules:{
-     ads, user, shared
+ads,
+user,
+shared,
+order
 }
 })
 
