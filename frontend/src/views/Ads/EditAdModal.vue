9805a93ba3d50aeb82c5292f4cf29ac0469571e8
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-06-19 02:27:48 +0300

    17.2/17.3

--- frontend/src/views/Ads/EditAdModal.vue
+++ frontend/src/views/Ads/EditAdModal.vue
@@ -1,48 +1,59 @@
 <template>
-    <v-dialog
-    v-model="modal"
-    width="400px">
-    <template v-slot:activator="{ props }">
-    <v-btn
-    v-bind="props"
-    color="warning">
-    Изменить</v-btn></template>
-    <v-card class="pa-3">
-    <v-row justify="center">
-    <v-col cols="12">
-    <v-card-title>
-    <h1 class="text--primary">Изменить</h1>
-    </v-card-title>
-    </v-col>
-    </v-row>
-    
-    <v-row justify="center">
-    <v-col cols="12">
-    <v-card-text>
-    Lorem.
-    </v-card-text>
-    </v-col>
-    </v-row>
-    
-    <v-row justify="center">
-    <v-col cols="12">
-    <v-card-actions>
-    <v-spacer></v-spacer>
-    <v-btn>Отменить</v-btn>
-    <v-btn color="success">Сохранить</v-btn>
-    </v-card-actions>
-    </v-col>
-    </v-row>
-    </v-card>
+    <v-dialog v-model="modal" width="400px">
+        <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" color="warning">
+                Изменить</v-btn></template>
+        <v-card class="pa-3">
+            <v-row justify="center">
+                <v-col cols="12">
+                    <v-card-title>
+                        <h1 class="text--primary">Изменить</h1>
+                    </v-card-title>
+                </v-col>
+            </v-row>
+
+            <v-row justify="center">
+                <v-col cols="12">
+                    <v-card-text>
+                        <v-text-field
+                            name="title"
+                            label="Название"
+                            type="text"
+                            v-model="editedTitle"
+                        >
+                        </v-text-field>
+                        <v-textarea
+                            name="desc"
+                            label="Описание"
+                            type="text"
+                            v-model="editedDesc"
+                            class="mb-3"
+                        ></v-textarea>
+                    </v-card-text>
+                </v-col>
+            </v-row>
+
+            <v-row justify="center">
+                <v-col cols="12">
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn>Отменить</v-btn>
+                        <v-btn color="success">Сохранить</v-btn>
+                    </v-card-actions>
+                </v-col>
+            </v-row>
+        </v-card>
     </v-dialog>
 </template>
-    
+
 <script>
-    export default {
+export default {
     data() {
-    return {
-    modal: false
+        return {
+            modal: false,
+            editedTitle: "",
+            editedDesc: ""
+        }
     }
-    }
-    }
+}
 </script>
